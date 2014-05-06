16fcc3b142321eca97e7eb612cec5acbd51e3532
sinan_nar <sinannar@Sinans-MacBook-Pro.local>
2014-05-06 04:11:44 +0300

    Completed the layout structure

--- sample_app/app/helpers/application_helper.rb
+++ sample_app/app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
-end
+
+	def title
+		base_title = "Sample App"
+		if @title.nil? #there should be question mark for nil 
+			base_title
+		else
+			" #{base_title} | #{ @title }"
+		end
+	end
+ end

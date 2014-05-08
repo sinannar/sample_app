d0f195806664b949c5952c4dc6bc1e14c6e6e7d2
sinan_nar <sinannar@Sinans-MacBook-Pro.local>
2014-05-08 20:43:48 +0300

    working index page

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
-  before_filter :authenticate , :only => [:edit,:update]
+  before_filter :authenticate , :only => [:edit,:index,:update]
   before_filter :correct_user , :only => [:edit,:update]
+
+  def index
+    @title = "All users"
+    @users = User.all    
+  end
 
   def show
   	@user = User.find(params[:id])

eb90f164a63d868b563755a5b6ed1e398bd4e243
sinan_nar <sinannar@Sinans-MacBook-Pro.local>
2014-05-08 03:37:47 +0300

    Finishing the password machinery 2

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -16,8 +16,8 @@ class User < ActiveRecord::Base
 		encryped_password == encrypt(submitted_password)
 	end 
 
-	class << self do
-		def self.authenticate(email, submitted_password)
+	class << self
+		def authenticate(email, submitted_password)
 			user = find_by_email(email)
 			return nil 	if user.nil?
 			return user if user.has_password?(submitted_password)

2b67edded2d27bcd81acae352845ca6fe9c9afb2
sinan_nar <sinannar@Sinans-MacBook-Pro.local>
2014-05-08 23:15:59 +0300

    Users deletion with are you sure

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_filter :authenticate , :only => [:edit,:index,:update]
+  before_filter :authenticate , :only => [:edit,:index,:update,:destroy]
   before_filter :correct_user , :only => [:edit,:update]
+  before_filter :admin_user, :only => :destroy
 
   def index
     @title = "All users"
@@ -30,7 +31,7 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])
     @title = "Edit User"
   end
 
@@ -42,8 +43,12 @@ class UsersController < ApplicationController
     else 
       @title = "Edit User"
       render 'edit'
-    end
-        
+    end   
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_path, :flash => { :success => "User destroyed." }
   end
 
 
@@ -56,4 +61,9 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
     end
+
+  def admin_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) if !current_user.admin? || current_user?(@user)
+  end
 end
--- sample_app/lib/tasks/sample_data.rake
+++ sample_app/lib/tasks/sample_data.rake
@@ -4,10 +4,11 @@ namespace :db do
 	desc "Fill database with sample data"
 	task :populate => :environment do
 		Rake::Task['db:reset'].invoke 
-		User.create!(	:name => "Eaxmple User",
+		admin = User.create!(	:name => "Eaxmple User",
 						:email => "example@user.com",
 						:password => "foobar",
 						:password_confirmation => "foobar")
+		admin.toggle!(:admin)
 		99.times do |n|
 			name = Faker::Name.name
 		    email = "example-#{n+1}@user.com"

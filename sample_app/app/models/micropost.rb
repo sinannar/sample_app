68b35777922f39224ba81694acbb5ad85a970fac
sinan_nar <sinannar@Sinans-MacBook-Pro.local>
2014-05-12 17:39:11 +0300

    micropost interface

--- sample_app/app/models/micropost.rb
+++ sample_app/app/models/micropost.rb
@@ -1,8 +1,8 @@
 class Micropost < ActiveRecord::Base
 	attr_accessible :content
 	belongs_to :user 
-	#validates :contet , :presence => true, :length => {:maximum => 380 }
-	#validates :user_id , :presence => true
-	#default_scope :order => 'microposts.create_at DESC'
-
+	validates :content , :presence => true, :length => {:maximum => 380 }
+	validates :user_id , :presence => true
+	default_scope :order => 'microposts.created_at DESC'
+  
 end
